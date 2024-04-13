ddf4f03b9c79208684cada5c47c193194e1371ae
Joe R <joe.roth.95@web.de>
2024-04-13 22:41:27 +0200

    outsourced everything to seperet plugins folder and split it into single files

--- init.lua
+++ init.lua
@@ -20,41 +20,5 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-    {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-	{
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-    }
-}	
-}
 
-local opts = {}
-
-
-require("lazy").setup(plugins, opts)
-
-
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-
-vim.keymap.set("n", "<C-n>", ":Neotree toggle filesystem reveal right<CR>")
-
-
-local configs = require("nvim-treesitter.configs")
-
-configs.setup({
-		ensure_installed = { "lua","javascript", "html", "typescript" },
-		sync_install = false,
-		highlight = { enable = true },
-		indent = { enable = true },  
-})
+require("lazy").setup("plugins")
