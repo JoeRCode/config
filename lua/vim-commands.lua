8ae8f31bb2a579c874db723c4664c5b65a1a8c2a
Joe R <joe.roth.95@web.de>
2024-04-14 15:22:12 +0200

    put in a color scheme and added/fixed some vim commands

--- lua/vim-commands.lua
+++ lua/vim-commands.lua
@@ -1,6 +1,9 @@
 vim.cmd("set number")
 vim.cmd("set relativenumber")
 vim.cmd("set autoindent")
-vim.cmd("set tabstop=4")
 vim.cmd("set mouse=a")
-vim.g.mapleader = " "
+vim.cmd("set expandtab")
+vim.cmd("set softtabstop=2")
+vim.cmd("set tabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "	
