8864f300f9f69ce5753874a06d5bb44a63a75321
Joe R <joe.roth.95@web.de>
2024-04-14 22:36:17 +0200

    removed eslint and installed eslint lsp in mason, set auto_install in treesitter for automatic language detection

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return  {
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
-        ensured_installed ={"lua", "javascript", "html", "typescript"} ,
+        auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
       })
