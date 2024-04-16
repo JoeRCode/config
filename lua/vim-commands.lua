d5132905012dad7f4ddcc7aad44c65fd1b4fffc5
Joe R <joe.roth.95@web.de>
2024-04-16 11:55:33 +0200

    added autotag, autopair and configured treesitter for it

--- lua/vim-commands.lua
+++ lua/vim-commands.lua
@@ -6,4 +6,8 @@ vim.cmd("set expandtab")
 vim.cmd("set softtabstop=2")
 vim.cmd("set tabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "	
+
+vim.g.mapleader = " "
+
+vim.opt.clipboard = "unnamedplus"
+
