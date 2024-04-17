9fdd4ebd54ed838a8b552168eb3982ee01f85d41
Joe R <joe.roth.95@web.de>
2024-04-17 17:48:16 +0200

    moved a global keymap to the commands file

--- lua/vim-commands.lua
+++ lua/vim-commands.lua
@@ -9,4 +9,5 @@ vim.cmd("set shiftwidth=2")
 
 vim.g.mapleader = " "
 vim.opt.clipboard = "unnamedplus"
+vim.keymap.set("n", "<C-n>", ":Neotree toggle filesystem reveal right<CR>", {})
 vim.keymap.set("n", "<C-s>", ":w<CR>", {})
